4897250f4371ecfc19791a3b95859d20b8b0bd0b
Thato <t.chitja369@gmail.com>
2024-03-17 10:52:20 +0200

    Define route handlers with functions

--- WebApiPractice/WebApiPractice/Program.cs
+++ WebApiPractice/WebApiPractice/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.HttpLogging;
+using System.Net.NetworkInformation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +14,62 @@ if (app.Environment.IsDevelopment())
     app.UseHttpLogging();
 }
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+
+var people = new List<Person>
+{
+    new("Tom", "Hanks"),
+    new("Denzel", "Washington"),
+    new("Leonardo", "DiCaprio"),
+    new("Al", "Pacino"),
+    new("Morgan", "Freeman"),
+};
+
+app.UseStaticFiles();
+app.UseRouting();
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/person", () => new Person("Andrew", "Lock"));
+app.MapGet("/person/{name}", (string name) => people.Where(p => p.FirstName.StartsWith(name)));
+app.MapGet("/error", () => "Sorry, an error occurred");
+
+app.MapGet("/fruit", () => Fruit.All);
+
+var getFruit = (string id) => Fruit.All[id];
+app.MapGet("/fruit/{id}", getFruit);
+
+app.MapPost("/fruit/{id}", Handlers.AddFruit);
+
+Handlers handlers = new();
+app.MapPut("/fuit/{id}", handlers.ReplaceFruit);
+
+app.MapDelete("/fruit/{id}", DeleteFruit);
 
 app.Run();
+
+void DeleteFruit(string id)
+{
+    Fruit.All.Remove(id);
+}
+
+record Fruit(string Name, int Stock)
+{
+    public static readonly Dictionary<string, Fruit> All = new();
+};
+
+class Handlers
+{
+    public void ReplaceFruit(string id, Fruit fruit)
+    {
+        Fruit.All[id] = fruit;
+    }
+
+    public static void AddFruit(string id, Fruit fruit)
+    {
+        Fruit.All.Add(id, fruit);
+    }
+}
 
 public record Person(string FirstName, string LastName);
